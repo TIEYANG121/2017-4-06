9dcfbbc5fab2514e243400a4cc1fa0f7af08ffe3
Your Name <you@example.com>
2017-04-24 16:24:22 +0800

    demo

---
+++ server/count.js
@@ -0,0 +1,10 @@
+var num = 1000;
+
+function count(m,n){
+	return m + n;
+}
+
+module.exports = {
+	num:num,
+	count:count
+};
---
+++ server/main.js
@@ -0,0 +1,6 @@
+var c = require('./count.js');
+
+var result = c.count(100,200);
+
+console.log(result);
+console.log(c.num);
